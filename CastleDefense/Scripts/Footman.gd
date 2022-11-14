5b1782d1b9fc40cc916015f7cf1cc18d3ab5aec8
zcodr <greninja1358@gmail.com>
2022-11-14 15:03:27 -0800

    Mac

---
+++ CastleDefense/Scripts/Footman.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+export var health : float;
+export var damage : float;
+export var speed : float;
+
+onready var vel : Vector2 = Vector2(speed, 0);
+
+func _process(delta):
+    move_and_slide(vel, Vector2.UP);

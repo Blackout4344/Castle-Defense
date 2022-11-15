77ec02fd52f1a50ac1c3582e709f037c06a3bb3d
zcodr <greninja1358@gmail.com>
2022-11-14 17:08:50 -0800

    Mac

---
+++ CastleDefense/Scripts/Archer.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+export var health : float;
+export var attack_cooldown : float; 
+export var speed : float;
+export var attack_range : float;
+
+onready var ArrowScene : PackedScene = preload("res://Scenes/Arrow.tscn");
+onready var vel : Vector2 = Vector2(speed, 0);
+var target : PhysicsBody2D = null;
+
+func _process(delta):
+    move_and_slide(vel, Vector2.UP);
+    
+func _on_AttackRange_body_entered(body : PhysicsBody2D):
+#    if (body.filename == "Path/To/Enemy/Scene(s)"):
+#        target = body;
+    pass;
+
+func _on_AttackCooldown_timeout():
+    if (target != null):
+        add_child(ArrowScene.instance());
---
+++ CastleDefense/Scripts/Arrow.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+export var damage : float;
+export var speed : float;
+
+onready var vel : Vector2 = Vector2(speed, 0);
+
+func _process(delta):
+    move_and_slide(vel, Vector2.UP);

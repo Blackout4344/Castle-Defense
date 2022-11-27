db0eb06995d943ef4500e6c26abed1d30484a62d
zcodr <greninja1358@gmail.com>
2022-11-27 12:39:01 -0800

    Mac

--- CastleDefense/Scripts/Archer.gd
+++ CastleDefense/Scripts/Archer.gd
@@ -10,13 +10,12 @@ onready var vel : Vector2 = Vector2(speed, 0);
 var target : PhysicsBody2D = null;
 
 func _process(delta):
-	move_and_slide(vel, Vector2.UP);
-	
+    move_and_slide(vel, Vector2.UP);
+    
 func _on_AttackRange_body_entered(body : PhysicsBody2D):
-#    if (body.filename == "Path/To/Enemy/Scene(s)"):
-#        target = body;
-	pass;
+    if (body.filename == "res://Scenes/EnemyPlaceholder.tscn"):
+        target = body;
 
 func _on_AttackCooldown_timeout():
-	if (target != null):
-		add_child(ArrowScene.instance());
+    if (target != null):
+        add_child(ArrowScene.instance());
--- CastleDefense/Scripts/Footman.gd
+++ CastleDefense/Scripts/Footman.gd
@@ -7,4 +7,4 @@ export var speed : float;
 onready var vel : Vector2 = Vector2(speed, 0);
 
 func _process(delta):
-	move_and_slide(vel, Vector2.UP);
+    move_and_slide(vel, Vector2.UP);

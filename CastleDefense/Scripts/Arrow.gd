b9f937047d19c6b2b8a70182a7af052341c8de7d
Blackout4344 <mohammad.khan6823@gmail.com>
2022-11-28 20:46:30 -0600

    Mac

--- CastleDefense/Scripts/Arrow.gd
+++ CastleDefense/Scripts/Arrow.gd
@@ -6,9 +6,9 @@ export var speed : float;
 var vel : Vector2;
 
 func _process(delta):
-    vel = move_and_slide(vel, Vector2.UP);
+	vel = move_and_slide(vel, Vector2.UP);
 
 func shoot(target : Vector2):
-    look_at(target);
-    vel = global_position.direction_to(target);
-    vel = vel.normalized() * speed;
+	look_at(target);
+	vel = global_position.direction_to(target);
+	vel = vel.normalized() * speed;

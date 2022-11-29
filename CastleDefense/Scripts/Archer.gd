24c46eb6bfa4236d9345023f503bc1366e215eb0
zcodr <greninja1358@gmail.com>
2022-11-28 18:20:08 -0800

    Worked on archer
    I made archer go brr and stuff

--- CastleDefense/Scripts/Archer.gd
+++ CastleDefense/Scripts/Archer.gd
@@ -2,20 +2,26 @@ extends KinematicBody2D
 
 export var health : float;
 export var attack_cooldown : float; 
-export var speed : float;
 export var attack_range : float;
 
 onready var ArrowScene : PackedScene = preload("res://Scenes/Arrow.tscn");
-onready var vel : Vector2 = Vector2(speed, 0);
 var target : PhysicsBody2D = null;
 
+func _ready():
+    $AttackCooldown.wait_time = attack_cooldown;
+    
 func _process(delta):
-	move_and_slide(vel, Vector2.UP);
-	
-func _on_AttackRange_body_entered(body : PhysicsBody2D):
-	if (body.filename == "res://Scenes/EnemyPlaceholder.tscn"):
-		target = body;
-
+    var enemy_x : int = 10000000;
+    for i in $AttackRange.get_overlapping_bodies():
+        if (i.filename != "res://Scenes/EnemyPlaceholder.tscn"):
+            continue;
+        if (i.global_position.x <= enemy_x):
+            enemy_x = i.global_position.x;
+            target = i;
+    
 func _on_AttackCooldown_timeout():
-	if (target != null):
-		add_child(ArrowScene.instance());
+    if (target != null):
+        var new_arrow : KinematicBody2D = ArrowScene.instance();
+        get_parent().add_child(new_arrow);
+        new_arrow.global_position = $Sprite/Bow.global_position;
+        new_arrow.shoot(target.global_position);
--- CastleDefense/Scripts/Arrow.gd
+++ CastleDefense/Scripts/Arrow.gd
@@ -3,7 +3,12 @@ extends KinematicBody2D
 export var damage : float;
 export var speed : float;
 
-onready var vel : Vector2 = Vector2(speed, 0);
+var vel : Vector2;
 
 func _process(delta):
-	move_and_slide(vel, Vector2.UP);
+    vel = move_and_slide(vel, Vector2.UP);
+
+func shoot(target : Vector2):
+    look_at(target);
+    vel = global_position.direction_to(target);
+    vel = vel.normalized() * speed;

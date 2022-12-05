227e4f2df024f80da7ac5b2bb7f94d193d66d0e5
Blackout4344 <mohammad.khan6823@gmail.com>
2022-12-05 13:56:26 -0600

    Mac

--- CastleDefense/Scripts/Archer.gd
+++ CastleDefense/Scripts/Archer.gd
@@ -5,20 +5,20 @@ export var attack_range : float;
 
 onready var ArrowScene : PackedScene = preload("res://Scenes/Arrow.tscn");
 var target : PhysicsBody2D = null;
-    
+	
 func _physics_process(delta):
-    var enemy_x : int = 10000000;
-    for i in $AttackRange.get_overlapping_bodies():
-        if (i.filename != "res://Scenes/Enemy.tscn"):
-            continue;
-        if (i.global_position.x <= enemy_x):
-            enemy_x = i.global_position.x;
-            target = i;
+	var enemy_x : int = 10000000;
+	for i in $AttackRange.get_overlapping_bodies():
+		if (i.filename != "res://Scenes/Enemy.tscn"):
+			continue;
+		if (i.global_position.x <= enemy_x):
+			enemy_x = i.global_position.x;
+			target = i;
 
 func _on_AttackCooldown_timeout():
-    if (weakref(target).get_ref()):
-        look_at(target.global_position);
-        var new_arrow : KinematicBody2D = ArrowScene.instance();
-        get_parent().add_child(new_arrow);
-        new_arrow.global_position = $Sprite/Bow.global_position;
-        new_arrow.shoot(target.global_position);
+	if (weakref(target).get_ref()):
+		look_at(target.global_position);
+		var new_arrow : KinematicBody2D = ArrowScene.instance();
+		get_parent().add_child(new_arrow);
+		new_arrow.global_position = $Sprite/Bow.global_position;
+		new_arrow.shoot(target.global_position);

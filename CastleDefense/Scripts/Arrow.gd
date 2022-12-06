ae9c0e708ad0c0cbce242d13dec4b986e1cbc2c8
zcodr <greninja1358@gmail.com>
2022-12-06 11:36:09 -0800

    Ditch
    Added Ditch which one-shots a 2 boys and but also destorys itself

--- CastleDefense/Scripts/Arrow.gd
+++ CastleDefense/Scripts/Arrow.gd
@@ -6,14 +6,14 @@ export var speed : float;
 var vel : Vector2;
 
 func _physics_process(delta):
-	vel = move_and_slide(vel, Vector2.UP);
+    vel = move_and_slide(vel, Vector2.UP);
 
 func shoot(target : Vector2):
-	look_at(target);
-	vel = global_position.direction_to(target);
-	vel = vel.normalized() * speed;
-	
+    look_at(target);
+    vel = global_position.direction_to(target);
+    vel = vel.normalized() * speed;
+    
 func _on_BodyCollisions_body_entered(body : PhysicsBody2D):
-	if (body.filename == "res://Scenes/Enemy.tscn" or body.filename == "res://Scenes/EnemyBase.tscn"):
-		body.health -= damage;
-		queue_free();
+    if (body.filename == "res://Scenes/Enemy.tscn" or body.filename == "res://Scenes/EnemyBase.tscn"):
+        body.health -= damage;
+        queue_free();
---
+++ CastleDefense/Scripts/Ditch.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+export var trap_hold : int;
+
+func _process(delta):
+    if (trap_hold <= 0):
+        queue_free();
+
+func _on_Trap_body_entered(body : PhysicsBody2D):
+    if (body.filename == "res://Scenes/Enemy.tscn"):
+        body.health = 0;
+        trap_hold -= 1;

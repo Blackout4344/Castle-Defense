8b372f4f657993c40af247e22dfd71a50d40a4bc
zcodr <greninja1358@gmail.com>
2022-12-05 16:57:00 -0800

    Mac

--- CastleDefense/Scripts/EnemyBase.gd
+++ CastleDefense/Scripts/EnemyBase.gd
@@ -5,9 +5,9 @@ export var spawn_rate : float;
 var EnemyScene : PackedScene = preload("res://Scenes/Enemy.tscn");
 
 func _ready():
-	$SpawnCooldown.wait_time = spawn_rate;
+    $SpawnCooldown.wait_time = spawn_rate;
 
 func _on_SpawnCooldown_timeout():
-	add_child(EnemyScene.instance());
-	spawn_rate *= 0.9;
-	$SpawnCooldown.wait_time = spawn_rate;
+    add_child(EnemyScene.instance());
+    spawn_rate *= 0.9;
+    $SpawnCooldown.wait_time = spawn_rate;
--- CastleDefense/Scripts/PlayerBase.gd
+++ CastleDefense/Scripts/PlayerBase.gd
@@ -4,27 +4,27 @@ var FootmanScene : PackedScene = preload("res://Scenes/Footman.tscn");
 var ArcherScene : PackedScene = preload("res://Scenes/Archer.tscn");
 
 func _process(delta):
-	$Units/Money.text = "Money: $" + str(Global.money)
+    $Units/Money.text = "Money: $" + str(Global.money)
 
 func _on_Footman_pressed():
-	if (Global.money >= 6):
-		Global.money -= 6;
-		var new_footman : KinematicBody2D = FootmanScene.instance();
-		new_footman.position = $FootmanLocation.position;
-		add_child(new_footman);
+    if (Global.money >= 6):
+        Global.money -= 6;
+        var new_footman : KinematicBody2D = FootmanScene.instance();
+        new_footman.position = $FootmanLocation.position;
+        add_child(new_footman);
 
 func _on_Archer_pressed():
-	if (Global.money >= 12):
-		Global.money -= 12;
-		var new_archer : KinematicBody2D = ArcherScene.instance();
-		new_archer.position = $ArcherLocation.position;
-		add_child(new_archer);
+    if (Global.money >= 12):
+        Global.money -= 12;
+        var new_archer : KinematicBody2D = ArcherScene.instance();
+        new_archer.position = $ArcherLocation.position;
+        add_child(new_archer);
 
 
 func _on_ShowUnitsShop_pressed():
-	$ShowUnitsShop.visible = false;
-	$Units.visible = true;
-	
+    $ShowUnitsShop.visible = false;
+    $Units.visible = true;
+    
 func _on_HideUnitsShop_pressed():
-	$Units.visible = false;
-	$ShowUnitsShop.visible = true;
+    $Units.visible = false;
+    $ShowUnitsShop.visible = true;

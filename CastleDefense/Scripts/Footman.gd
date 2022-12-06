edd88c71e0235a8d49b05a15062e94c3900c2620
Blackout4344 <mohammad.khan6823@gmail.com>
2022-12-05 18:47:22 -0600

    Mac

--- CastleDefense/Scripts/Footman.gd
+++ CastleDefense/Scripts/Footman.gd
@@ -10,27 +10,27 @@ onready var vel : Vector2 = Vector2(speed, 0);
 var target : PhysicsBody2D = null;
 
 func _ready():
-    $HealthBar.max_value = health;
+	$HealthBar.max_value = health;
 
 func _physics_process(delta):
-    $AnimatedSprite.play("New Anim")
-    if (health <= 0):
-        queue_free();
-    $HealthBar.value = health;
-    move_and_slide(vel, Vector2.UP);
-    var enemy_x : int = 10000000;
-    for i in $BodyCollisions.get_overlapping_bodies():
-        if (i.filename != "res://Scenes/Enemy.tscn"):
-            continue;
-        if (i.global_position.x <= enemy_x):
-            enemy_x = i.global_position.x;
-            target = i;
-            
+	$AnimatedSprite.play("New Anim")
+	if (health <= 0):
+		queue_free();
+	$HealthBar.value = health;
+	move_and_slide(vel, Vector2.UP);
+	var enemy_x : int = 10000000;
+	for i in $BodyCollisions.get_overlapping_bodies():
+		if (i.filename != "res://Scenes/Enemy.tscn"):
+			continue;
+		if (i.global_position.x <= enemy_x):
+			enemy_x = i.global_position.x;
+			target = i;
+			
 func _on_AttackCooldown_timeout():
-    if (weakref(target).get_ref()):
-        $AnimatedSprite.play("attack")
-        
-        look_at(target.global_position);
-        target.health -= damage;
-        
+	if (weakref(target).get_ref()):
+		$AnimatedSprite.play("attack")
+		
+		look_at(target.global_position);
+		target.health -= damage;
+		
 

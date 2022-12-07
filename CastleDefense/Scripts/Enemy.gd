f27782ed1381469a93ef7e1bda45d21165c8712d
Blackout4344 <mohammad.khan6823@gmail.com>
2022-12-06 18:56:43 -0600

    Fonts

--- CastleDefense/Scripts/Enemy.gd
+++ CastleDefense/Scripts/Enemy.gd
@@ -8,22 +8,22 @@ onready var vel : Vector2 = Vector2(-speed, 0);
 var target : PhysicsBody2D = null;
 
 func _ready():
-    $HealthBar.max_value = health;
+	$HealthBar.max_value = health;
 
 func _physics_process(delta):
-    if (health <= 0):
-        Global.money += 2;
-        queue_free();
-    $HealthBar.value = health;
-    move_and_slide(vel, Vector2.UP);
-    var player_x : int = -10000000;
-    for i in $BodyCollisions.get_overlapping_bodies():
-        if (i.filename != "res://Scenes/Footman.tscn" && i.filename != "res://Scenes/PlayerBase.tscn"):
-            continue;
-        if (i.global_position.x >= player_x):
-            player_x = i.global_position.x;
-            target = i;
-            
+	if (health <= 0):
+		Global.money += 2;
+		queue_free();
+	$HealthBar.value = health;
+	move_and_slide(vel, Vector2.UP);
+	var player_x : int = -10000000;
+	for i in $BodyCollisions.get_overlapping_bodies():
+		if (i.filename != "res://Scenes/Footman.tscn" && i.filename != "res://Scenes/PlayerBase.tscn"):
+			continue;
+		if (i.global_position.x >= player_x):
+			player_x = i.global_position.x;
+			target = i;
+			
 func _on_AttackCooldown_timeout():
-    if (weakref(target).get_ref()):
-        target.health -= damage;
+	if (weakref(target).get_ref()):
+		target.health -= damage;

a252d7d8551170822abfca2abbf44fe2fb09b760
Blackout4344 <mohammad.khan6823@gmail.com>
2022-12-12 12:05:47 -0600

    Backround/Cloud

--- CastleDefense/Scripts/EnemyBase.gd
+++ CastleDefense/Scripts/EnemyBase.gd
@@ -7,17 +7,17 @@ export var health : float;
 var EnemyScene : PackedScene = preload("res://Scenes/Enemy.tscn");
 
 func _ready():
-    $SpawnCooldown.wait_time = spawn_rate;
-    $HealthBar.max_value = health;
-    
+	$SpawnCooldown.wait_time = spawn_rate;
+	$HealthBar.max_value = health;
+	
 func _process(delta):
-    if (health <= 0):
-        queue_free();
-    $HealthBar.value = health;
+	if (health <= 0):
+		queue_free();
+	$HealthBar.value = health;
 
 func _on_SpawnCooldown_timeout():
-    var new_enemy : KinematicBody2D = EnemyScene.instance();
-    new_enemy.position = $EnemyLocation.position;
-    add_child(new_enemy);
-    spawn_rate *= 0.9;
-    $SpawnCooldown.wait_time = spawn_rate;
+	var new_enemy : KinematicBody2D = EnemyScene.instance();
+	new_enemy.position = $EnemyLocation.position;
+	add_child(new_enemy);
+	spawn_rate *= 0.9;
+	$SpawnCooldown.wait_time = spawn_rate;
--- CastleDefense/Scripts/PlayerBase.gd
+++ CastleDefense/Scripts/PlayerBase.gd
@@ -8,39 +8,39 @@ var ArcherScene : PackedScene = preload("res://Scenes/Archer.tscn");
 var DitchScene : PackedScene = preload("res://Scenes/Ditch.tscn");
 
 func _process(delta):
-    $Units/Money.text = "Money: $" + str(Global.money)
-    if (health <= 0):
-        queue_free();
-    $HealthBar.value = health;
+	$Units/Money.text = "Money: $" + str(Global.money)
+	if (health <= 0):
+		queue_free();
+	$HealthBar.value = health;
 
 func _ready():
-    $HealthBar.max_value = health;
+	$HealthBar.max_value = health;
 
 func _on_ShowUnitsShop_pressed():
-    $ShowUnitsShop.visible = false;
-    $Units.visible = true;
-    
+	$ShowUnitsShop.visible = false;
+	$Units.visible = true;
+	
 func _on_HideUnitsShop_pressed():
-    $Units.visible = false;
-    $ShowUnitsShop.visible = true;
+	$Units.visible = false;
+	$ShowUnitsShop.visible = true;
 
 func _on_Footman_pressed():
-    if (Global.money >= 6):
-        Global.money -= 6;
-        var new_footman : KinematicBody2D = FootmanScene.instance();
-        new_footman.position = $FootmanLocation.position;
-        add_child(new_footman);
+	if (Global.money >= 6):
+		Global.money -= 6;
+		var new_footman : KinematicBody2D = FootmanScene.instance();
+		new_footman.position = $FootmanLocation.position;
+		add_child(new_footman);
 
 func _on_Archer_pressed():
-    if (Global.money >= 20):
-        Global.money -= 20;
-        var new_archer : KinematicBody2D = ArcherScene.instance();
-        new_archer.position = $ArcherLocation.position;
-        add_child(new_archer);
-        
+	if (Global.money >= 20):
+		Global.money -= 20;
+		var new_archer : KinematicBody2D = ArcherScene.instance();
+		new_archer.position = $ArcherLocation.position;
+		add_child(new_archer);
+		
 func _on_Ditch_pressed():
-    if (Global.money >= 12):
-        Global.money -= 12;
-        var new_ditch : StaticBody2D = DitchScene.instance();
-        new_ditch.position = $DitchLocation.position;
-        add_child(new_ditch);
+	if (Global.money >= 12):
+		Global.money -= 12;
+		var new_ditch : StaticBody2D = DitchScene.instance();
+		new_ditch.position = $DitchLocation.position;
+		add_child(new_ditch);

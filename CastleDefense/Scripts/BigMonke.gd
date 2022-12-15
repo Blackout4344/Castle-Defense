6b82e95050e5811f0393fec0b5b6d19ef7276940
zcodr <greninja1358@gmail.com>
2022-12-14 16:36:54 -0800

    Added BigMonke Unit

--- CastleDefense/Scripts/BigMonke.gd
+++ CastleDefense/Scripts/BigMonke.gd
@@ -4,7 +4,7 @@ export var health : float;
 export var damage : float;
 export var speed : float;
 
-onready var vel : Vector2 = Vector2(-speed, 0);
+onready var vel : Vector2 = Vector2(speed, 0);
 var target : PhysicsBody2D = null;
 
 func _ready():
@@ -12,20 +12,26 @@ func _ready():
 
 func _physics_process(delta):
     if (health <= 0):
-        Global.money += 2;
         queue_free();
     $HealthBar.value = health;
     move_and_slide(vel, Vector2.UP);
-    var player_x : int = -10000000;
+    var enemy_x : int = 10000000;
     for i in $BodyCollisions.get_overlapping_bodies():
-        if (i.filename != "res://Scenes/Footman.tscn" && i.filename != "res://Scenes/PlayerBase.tscn" && i.filename != "res://Scenes/Knight.tscn" && i.filename != "res://Scenes/BigMonke.tscn"):
-            vel = Vector2(-speed, 0);
+        if (i.filename != "res://Scenes/Enemy.tscn" && i.filename != "res://Scenes/EnemyBase.tscn"):
+            vel = Vector2(speed, 0);
             continue;
-        if (i.global_position.x >= player_x):
-            player_x = i.global_position.x;
+        if (i.global_position.x <= enemy_x):
+            enemy_x = i.global_position.x;
             target = i;
         vel = Vector2(0, 0);
             
 func _on_AttackCooldown_timeout():
     if (weakref(target).get_ref()):
         target.health -= damage;
+        
+func _on_AnimatedSprite_animation_finished():
+    if (weakref(target).get_ref()):
+        if ($BodyCollisions.overlaps_body(target)):
+            $AnimatedSprite.play("attack");
+    else:
+        $AnimatedSprite.play("run");
--- CastleDefense/Scripts/PlayerBase.gd
+++ CastleDefense/Scripts/PlayerBase.gd
@@ -6,6 +6,7 @@ var FootmanScene : PackedScene = preload("res://Scenes/Footman.tscn");
 var ArcherScene : PackedScene = preload("res://Scenes/Archer.tscn");
 var DitchScene : PackedScene = preload("res://Scenes/Ditch.tscn");
 var KnightScene : PackedScene = preload("res://Scenes/Knight.tscn");
+var BigMonkeScene : PackedScene = preload("res://Scenes/BigMonke.tscn");
 
 func _process(delta):
     $Units/Money.text = "Money: $" + str(Global.money)
@@ -55,3 +56,11 @@ func _on_Knight_pressed():
         new_knight.position = $KnightLocation.position;
         new_knight.z_index = -1;
         add_child(new_knight);
+
+func _on_BigMonke_pressed():
+    if (Global.money >= 30):
+        Global.money -= 30;
+        var new_bigmonke : KinematicBody2D = BigMonkeScene.instance();
+        new_bigmonke.position = $BigMonkeLocation.position;
+        new_bigmonke.z_index = -1;
+        add_child(new_bigmonke);

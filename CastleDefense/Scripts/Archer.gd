26c3f1c1b796d3b590a8466844586b7f739610d2
Blackout4344 <mohammad.khan6823@gmail.com>
2023-02-26 16:32:27 -0600

    Mac

--- CastleDefense/Scripts/Archer.gd
+++ CastleDefense/Scripts/Archer.gd
@@ -4,24 +4,24 @@ export var attack_range : float;
 
 onready var ArrowScene : PackedScene = preload("res://Scenes/Arrow.tscn");
 var target : PhysicsBody2D = null;
-    
+	
 func _physics_process(delta):
-    var enemy_x : int = 10000000;
-    for i in $AttackRange.get_overlapping_bodies():
-        if (i.filename != "res://Scenes/EnemyFootman.tscn"):
-            continue;
-        if (i.global_position.x <= enemy_x):
-            enemy_x = i.global_position.x;
-            target = i;
+	var enemy_x : int = 10000000;
+	for i in $AttackRange.get_overlapping_bodies():
+		if (i.filename != "res://Scenes/EnemyFootman.tscn"):
+			continue;
+		if (i.global_position.x <= enemy_x):
+			enemy_x = i.global_position.x;
+			target = i;
 
 func _on_AttackCooldown_timeout():
-    if (weakref(target).get_ref()):
-        $Sprite.play("shoot");
-        look_at(target.global_position);
-        var new_arrow : KinematicBody2D = ArrowScene.instance();
-        get_parent().add_child(new_arrow);
-        new_arrow.global_position = $ArrowLocation.global_position;
-        new_arrow.shoot(target.global_position);
-        
+	if (weakref(target).get_ref()):
+		$Sprite.play("shoot");
+		look_at(target.global_position);
+		var new_arrow : KinematicBody2D = ArrowScene.instance();
+		get_parent().add_child(new_arrow);
+		new_arrow.global_position = $ArrowLocation.global_position;
+		new_arrow.shoot(target.global_position);
+		
 func _on_Sprite_animation_finished():
-    $Sprite.stop();
+	$Sprite.stop();
--- CastleDefense/Scripts/Knight.gd
+++ CastleDefense/Scripts/Knight.gd
@@ -8,29 +8,29 @@ onready var vel : Vector2 = Vector2(speed, 0);
 var target : PhysicsBody2D = null;
 
 func _ready():
-    $HealthBar.max_value = health;
+	$HealthBar.max_value = health;
 
 func _physics_process(delta):
-    if (health <= 0):
-        queue_free();
-    $HealthBar.value = health;
-    move_and_slide(vel, Vector2.UP);
-    var enemy_x : int = 10000000;
-    for i in $BodyCollisions.get_overlapping_bodies():
-        if (i.filename != "res://Scenes/EnemyFootman.tscn" && i.filename != "res://Scenes/EnemyBase.tscn"):
-            vel = Vector2(speed, 0);
-            continue;
-        if (i.global_position.x <= enemy_x):
-            enemy_x = i.global_position.x;
-            target = i;
-        vel = Vector2(0, 0);
-            
+	if (health <= 0):
+		queue_free();
+	$HealthBar.value = health;
+	move_and_slide(vel, Vector2.UP);
+	var enemy_x : int = 10000000;
+	for i in $BodyCollisions.get_overlapping_bodies():
+		if (i.filename != "res://Scenes/EnemyFootman.tscn" && i.filename != "res://Scenes/EnemyBase.tscn"):
+			vel = Vector2(speed, 0);
+			continue;
+		if (i.global_position.x <= enemy_x):
+			enemy_x = i.global_position.x;
+			target = i;
+		vel = Vector2(0, 0);
+			
 func _on_AttackCooldown_timeout():
-    if (weakref(target).get_ref()):
-        target.health -= damage;
+	if (weakref(target).get_ref()):
+		target.health -= damage;
 func _on_AnimatedSprite_animation_finished():
-    if (weakref(target).get_ref()):
-        if ($BodyCollisions.overlaps_body(target)):
-            $AnimatedSprite.play("attack");
-    else:
-        $AnimatedSprite.play("run");
+	if (weakref(target).get_ref()):
+		if ($BodyCollisions.overlaps_body(target)):
+			$AnimatedSprite.play("attack");
+	else:
+		$AnimatedSprite.play("run");
